044cbaa2663dc0cfa9f86db53de4079bb4d44ef3
RNGDelak <phamlehaibang331@gmail.com>
2026-07-10 17:58:08 +0700

    Update Conversion BMS - 2 shifted OCF v2.js

--- Conv/Conversion BMS - 2 shifted OCF v2.js
+++ Conv/Conversion BMS - 2 shifted OCF v2.js
@@ -52,7 +52,7 @@ function conv(matrix) {
     return lt(firstTerm(x)[1], firstTerm(y)[1]);
   }
 
-  function gt(x, y) { return !(x == y && lt(x, y)) }
+  function gt(x, y) { return !(x == y || lt(x, y)) }
 
   function add(x, y) {
     if (x == '0') { return y; }
